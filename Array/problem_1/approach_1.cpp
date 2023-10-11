4a605a102e2d7e8676115904cc39269479a433b1
amriteshbytesview <amritesh@bytesview.com>
2023-10-11 19:49:53 +0530

    updated

--- Array/problem_1/approach_1.cpp
+++ Array/problem_1/approach_1.cpp
@@ -1,17 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int arr[] = {8, 7, 2, 5, 3, 1, 9};
-    int arr_len = sizeof(arr)/sizeof(arr[0]);
-    int target = 10;
 
+void FindPair(int arr[], int arr_len, int target){
 
-    for(int i=0; i<arr_len; i++){
-        for(int j=i+1; j<arr_len; j++){
-            if(arr[i]+arr[j]==target){
-                cout<<arr[i]<<" "<<arr[j]<<endl;
+        for(int i=0; i<arr_len; i++){
+            for(int j=i+1; j<arr_len; j++){
+                if(arr[i]+arr[j]==target){
+                    cout<<arr[i]<<" "<<arr[j]<<endl;
+                    return;
+                }
             }
         }
-    }
+    cout<<"Pair not found"<<endl;
+    
+}
+
+
+
+int main(){
+    int arr[] = {5, 2, 6, 8, 1, 6, 9};
+    int arr_len = sizeof(arr)/sizeof(arr[0]);
+    int target = 12;
+
+    FindPair(arr, arr_len, target);
 }
