fd05e72f3bfe28ec69d061de83603a2d85bfde66
amriteshbytesview <amritesh@bytesview.com>
2023-10-20 10:51:31 +0530

    update

---
+++ Array/problem_8/approach_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void SortByOneSwap(int arr[], int arr_len){
+    int* ptr1;
+    int* ptr2;
+    for(int i=0; i<arr_len;i++){
+
+    }
+}
+
+int main(){
+    int arr[] = {3, 8, 6, 7, 5, 9};
+    int arr_len = sizeof(arr)/(arr[0]);
+
+    SortByOneSwap(arr, arr_len);
+}
+
